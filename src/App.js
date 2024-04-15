677079210720b747b60476b5fe7816bf4fd5dfdf
hs223nr <hs223nr@student.lnu.se>
2024-04-15 13:53:27 +0200

    fix build?

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import Header from './components/header/header';
 import About from './components/about/about';
 import Resume from './components/resume/resume';
-import OtherSkills from './components/skills/skills'
 import Portfolio from './components/portfolio/portfolio';
 import Footer from './components/footer/footer';
 import resumeData from './resumeData';
